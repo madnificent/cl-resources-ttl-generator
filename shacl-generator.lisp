aa87f645c9dbb7e640587fea5762bd2da4d285bb
Aad Versteden <madnificent@gmail.com>
2026-01-12 15:39:17 +0100

    Less blank nodes

--- shacl-generator.lisp
+++ shacl-generator.lisp
@@ -1,7 +1,5 @@
 (in-package :shacl-generator)
 
-(mu-cl-resources::add-prefix "cms" "http://mu.semte.ch/vocabulary/cms/")
-
 (declaim (optimize (debug 3) (speed 0)))
 
 (defun all-resources ()
@@ -54,12 +52,24 @@
           *service-base-uri*
           (resource-uri-postfix resource)))
 
+(defun resource-property-uri (resource slot)
+  (format nil "<~A/shapes/~A/properties/~A>"
+          *service-base-uri*
+          (resource-uri-postfix resource)
+          (mu-cl-resources::json-key slot)))
+
+(defun resource-property-inverse-path-uri (resource slot)
+  (format nil "<~A/shapes/~A/properties/~A/paths>"
+          *service-base-uri*
+          (resource-uri-postfix resource)
+          (mu-cl-resources::json-key slot)))
+
 (defun make-ttl-prefixes ()
   "Constructs the prefixes which may have been used in the model."
-  (format nil "~&~{@prefix ~A: <~A>.~%~}@prefix dc: <http://purl.org/dc/elements/1.1/>.~%@prefix dct: <http://purl.org/dc/terms/>.~%@prefix sh: <shaclhttp://www.w3.org/ns/shacl#>.~%@prefix ext: <http://mu.semte.ch/vocabluries/ext/>.~%@prefix mu: <http://mu.semte.ch/vocabularies/core/>.~%"
+  (format nil "~&~{@prefix ~A: <~A>.~%~}@prefix dc: <http://purl.org/dc/elements/1.1/>.~%@prefix dct: <http://purl.org/dc/terms/>.~%@prefix sh: <http://www.w3.org/ns/shacl#>.~%@prefix ext: <http://mu.semte.ch/vocabluries/ext/>.~%@prefix mu: <http://mu.semte.ch/vocabularies/core/>.~%"
           (loop for (prefix . iri)
-             in (cl-fuseki:get-prefix-alist)
-             append (list prefix iri))))
+                  in (cl-fuseki:get-prefix-alist)
+                append (list prefix iri))))
 
 (defun make-ttl-service-description ()
   "Constructs the description of the service as per diSHCALed."
@@ -119,53 +129,53 @@
   (format nil "~{~@[~A~%~]~}~%"
           `(,(resource-shape-uri resource)
             "  a sh:NodeShape;"
-            ,(format nil "  sh:targetClass ~A;" (mu-cl-resources::ld-class resource))
+            ,(format nil "  sh:targetClass ~A." (mu-cl-resources::ld-class resource))
             ;; the properties
-            ,(when (mu-cl-resources::ld-properties resource)
-               (format
-                nil
-                "~@[  sh:property [~{~&~A~%~^  ] , [~}  ];~]"
-                (loop for slot in (mu-cl-resources::ld-properties resource)
-                      collect
-                      (format
-                       nil
-                       "    sh:path ~A;~%    sh:minCount ~A~@[;~%    sh:maxCount ~A;~]~@[;~%    sh:dataType ~A~]"
-                       (mu-cl-resources::ld-property slot)
-                       (if (mu-cl-resources::required-p slot) 1 0)
-                       (unless (find (mu-cl-resources::resource-type slot) '(:string-set :uri-set :language-string-set))
-                         ;; these have multiple values, in whichcase there may not be an upper bound
-                         1)
-                       (case (mu-cl-resources::resource-type slot)
-                         (:string "xsd:string")
-                         (:number "xsd:decimal") ; could be float or integer too
-                         (:integer "xsd:integer")
-                         (:float "xsd:float")
-                         (:boolean "xsd:boolean")
-                         (:date "xsd:date")
-                         (:datetime "xsd:dateTime")
-                         (:time "xsd:time")
-                         (:string-set "xsd:string")
-                         (:language-string "xsd:string")
-                         (:language-string-set "xsd:string")
-                         (:g-year "xsd:gYear")
-                         (:geometry "geo:wktLiteral")
-                         (otherwise nil)  ; :url and :uri-set will not get a dataType
-                         )))))
+            ,@(loop for slot in (mu-cl-resources::ld-properties resource)
+                    for shacl-property-uri = (resource-property-uri resource slot)
+                    collect
+                    (format nil "~A sh:property ~A.~%~A~%  sh:path ~A;~%  sh:minCount ~A~@[;~%  sh:maxCount ~A~]~@[;~%  sh:dataType ~A~]."
+                            (resource-shape-uri resource)
+                            shacl-property-uri
+                            shacl-property-uri
+                            (mu-cl-resources::ld-property slot)
+                            (if (mu-cl-resources::required-p slot) 1 0)
+                            (unless (find (mu-cl-resources::resource-type slot) '(:string-set :uri-set :language-string-set))
+                              ;; these have multiple values, in whichcase there may not be an upper bound
+                              1)
+                            (case (mu-cl-resources::resource-type slot)
+                              (:string "xsd:string")
+                              (:number "xsd:decimal") ; could be float or integer too
+                              (:integer "xsd:integer")
+                              (:float "xsd:float")
+                              (:boolean "xsd:boolean")
+                              (:date "xsd:date")
+                              (:datetime "xsd:dateTime")
+                              (:time "xsd:time")
+                              (:string-set "xsd:string")
+                              (:language-string "xsd:string")
+                              (:language-string-set "xsd:string")
+                              (:g-year "xsd:gYear")
+                              (:geometry "geo:wktLiteral")
+                              (otherwise nil) ; :url and :uri-set will not get a dataType
+                              )))
             ;; the relations
-            ,(when (mu-cl-resources::all-links resource)
-               (format
-                nil
-                "~@[  sh:property [~{~&~A~%~^  ] , [~}  ];~]"
-                (loop for link in (mu-cl-resources::all-links resource)
-                      collect
-                      (format
-                       nil
-                       "    sh:path ~:[~A~;[ sh:inversePath ~A]~];~%    sh:minCount 0~@[;~%    sh:maxCount ~A~];~%    sh:class ~A;~%    sh:nodeKind sh:IRI"
-                       (mu-cl-resources::inverse-p link)
-                       (mu-cl-resources::ld-link link)
-                       (and (typep link 'mu-cl-resources::has-one-link) 1)
-                       (mu-cl-resources::ld-class (mu-cl-resources::referred-resource link))))))
-            "sh:closed false.")))
+            ,@(loop for link in (mu-cl-resources::all-links resource)
+                    for shacl-property-uri = (resource-property-uri resource link)
+                    for inverse-path-uri = (resource-property-inverse-path-uri resource link)
+                    append
+                    `(,(format nil "~A sh:property ~A.~%~A~%  sh:path ~A;~%  sh:minCount 0~@[;~%  sh:maxCount ~A~];~%  sh:class ~A;~%  sh:nodeKind sh:IRI."
+                               (resource-shape-uri resource)
+                               shacl-property-uri
+                               shacl-property-uri
+                               (if (mu-cl-resources::inverse-p link)
+                                   inverse-path-uri
+                                   (mu-cl-resources::ld-link link))
+                               (and (typep link 'mu-cl-resources::has-one-link) 1)
+                               (mu-cl-resources::ld-class (mu-cl-resources::referred-resource link)))
+                      ,@(when (mu-cl-resources::inverse-p link)
+                          (list (format nil "~A sh:inversePath ~A." inverse-path-uri (mu-cl-resources::ld-link link))))))
+            ,(format nil "~A sh:closed false." (resource-shape-uri resource)))))
 
 (defun make-full-shape ()
   (format nil "~A~%  a sh:NodeShape;~%  sh:or (~{~%    ~A~})."
